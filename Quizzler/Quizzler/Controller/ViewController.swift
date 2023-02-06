4a743cedadb878f0ebc3bdc326310a4e33c32d74
Sudur Sugunushev <sudursugunushev@gmail.com>
2023-02-06 16:21:00 +0600

    Quizzler is started

---
+++ Quizzler/Quizzler/Controller/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Судур Сугунушев on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+  
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateUI() {
+        
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+    
+}
+
