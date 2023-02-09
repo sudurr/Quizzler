f281d255785aa59659a2125a23fe1a559ad92d4e
Sudur Sugunushev <sudursugunushev@gmail.com>
2023-02-09 17:39:47 +0600

    1st part added

--- Quizzler/Quizzler/Controller/ViewController.swift
+++ Quizzler/Quizzler/Controller/ViewController.swift
@@ -9,42 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-  
-    
-    var quizBrain = QuizBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        updateUI()
-    }
-
-    @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
-        let userAnswer = sender.currentTitle!
-        let userGotItRight = quizBrain.checkAnswer(userAnswer)
-        
-        if userGotItRight {
-            sender.backgroundColor = UIColor.green
-        } else {
-            sender.backgroundColor = UIColor.red
-        }
-        
-        quizBrain.nextQuestion()
-        
-        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
-        
-    }
-    
-    @objc func updateUI() {
-        
-        questionLabel.text = quizBrain.getQuestionText()
-        progressBar.progress = quizBrain.getProgress()
-        
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
     }
     
 }
-
